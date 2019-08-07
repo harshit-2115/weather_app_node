e6ec73589ba9ff6b770e520488dd651e44574503
harshit-2115 <harshit.2115@gmail.com>
2019-08-07 17:18:01 +0530

    forecast.io API for weather details

--- app.js
+++ app.js
@@ -20,7 +20,31 @@ request({
 	url: `https://us1.locationiq.com/v1/search.php?key=951c653da2ebd4&q=${encodedAddress}&format=json`,
 	json: true
 }, (error,response,body) => {
-	// console.log(`Address : ${body.result[0].formatted_address}`);
-	console.log(`Latitude : ${body[0].lat}`);
-	console.log(`Longitude : ${body[0].lon}`);
+	if (body.error) {
+		console.log('Unable to fetch geocodes');
+	}
+	else {
+
+
+	place = body[0].display_name;
+	lat = body[0].lat;
+	lon = body[0].lon;
+
+	console.log(`Place : ${place}`);	
+	console.log(`Latitude : ${lat}`);
+	console.log(`Longitude : ${lon}`);
+	request({
+		url : `https://api.darksky.net/forecast/11530e225f5d076483a750f657f8f6bb/${lat},${lon}`,
+		json : true
+},(error,response,body) => {
+		console.log(response)
+		temperature = body.currently.temperature;
+		humidity = body.currently.humidity;
+		windspeed = body.currently.windSpeed;
+		console.log(`\nTemperature : ${temperature}`);
+		console.log(`Humidity : ${humidity}`);
+		console.log(`Windspeed : ${windspeed}`);
+	});
+	}
 });
+
