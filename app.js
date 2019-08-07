022f0cdf57979480e1985d777bbd59a16187014b
harshit-2115 <harshit.2115@gmail.com>
2019-08-07 16:30:02 +0530

    latitude and longitude using Locationhq api

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const request = require('request')
-const fs = require('fs');
-const os = require('os');
+const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help','h')
+	.argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-	url: 'http://www.google.com',
+	url: `https://us1.locationiq.com/v1/search.php?key=951c653da2ebd4&q=${encodedAddress}&format=json`,
 	json: true
 }, (error,response,body) => {
-	console.log(body);
+	// console.log(`Address : ${body.result[0].formatted_address}`);
+	console.log(`Latitude : ${body[0].lat}`);
+	console.log(`Longitude : ${body[0].lon}`);
 });
